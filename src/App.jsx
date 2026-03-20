1a57a39be0586254524f3131afa9240526718285
Himanshudabhi341 <himanshudabhi012@gmail.com>
2026-03-20 23:31:12 +0530

    create one landing page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import NexgenzLanding from './pages/landing/NexgenzLanding'
+
+function App() {
+
+  return (
+    <>
+      <NexgenzLanding />
+    </>
+  )
+}
+
+export default App
